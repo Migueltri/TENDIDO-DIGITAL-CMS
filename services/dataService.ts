f63f33e73d818e65f4e94dc04fd574c92b3d98b8
Migueltri <mitrijurrea@gmail.com>
2026-03-10 23:43:50 +0100

    Update dataService.ts

--- services/dataService.ts
+++ services/dataService.ts
@@ -264,17 +264,17 @@ export const restoreArticle = (id: string, skipSync = false): boolean => {
 // Al final de src/services/dataService.ts
 // import backupData from '../../backup.json';
 
-//export const injectBackup = () => {
-  try {
-    if (backupData && backupData.articles) {
-      localStorage.setItem('articles', JSON.stringify(backupData.articles));
-      localStorage.setItem('authors', JSON.stringify(backupData.authors || []));
-      localStorage.setItem('archivedArticles', JSON.stringify(backupData.archivedArticles || []));
-      return true;
-    }
-    return false;
-  } catch (e) {
-    console.error("Error inyectando:", e);
-    return false;
-  }
-};
+// export const injectBackup = () => {
+ // try {
+   //   if (backupData && backupData.articles) {
+     //   localStorage.setItem('articles', JSON.stringify(backupData.articles));
+   //     localStorage.setItem('authors', JSON.stringify(backupData.authors || []));
+   //     localStorage.setItem('archivedArticles', JSON.stringify(backupData.archivedArticles || []));
+  //      return true;
+   //   }
+ //     return false;
+ //   } catch (e) {
+ //     console.error("Error inyectando:", e);
+ //     return false;
+ //   }
+ // };
